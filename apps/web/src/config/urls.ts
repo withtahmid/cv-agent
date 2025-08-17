f3a0022fb887f28d77f58a9bcbf4701b012cba6c
withtahmid <withtahmid@gmail.com>
2025-08-18 01:23:52 +0600

    given backen uro and pubblic folder

--- apps/web/src/config/urls.ts
+++ apps/web/src/config/urls.ts
@@ -1,10 +1,10 @@
 import config from ".";
 
-export const baseBackendURL =
-    config.NODE_ENV === "development"
-        ? `${window.location.protocol}//${window.location.hostname}:${config.SERVER_PORT}`
-        : config.NODE_ENV === "test"
-          ? "test url"
-          : config.NODE_ENV === "production"
-            ? "production url"
-            : "invalid url";
+export const baseBackendURL = "https://mailreadreceipts.vercel.app";
+// config.NODE_ENV === "development"
+//     ? `${window.location.protocol}//${window.location.hostname}:${config.SERVER_PORT}`
+//     : config.NODE_ENV === "test"
+//       ? "test url"
+//       : config.NODE_ENV === "production"
+//         ? "production url"
+//         : "invalid url";
